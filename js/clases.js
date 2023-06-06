5a75acc2adf9c02e20eaad6444736e97b5c076b2
Pedro Aznarez <pedroaznarez12@gmail.com>
2023-06-06 16:57:25 -0300

    actulizado clases

--- js/clases.js
+++ js/clases.js
@@ -8,6 +8,30 @@ class Sistema {
   obtenerUsuarios() {
     return this.usuarios;
   }
+
+  // Ver la clase o ver luego que no esta referenciado que el profe ya estaba yendo al palo
+  usuarioRepetido(nombre){
+    let estaRepetido = false;
+    for(let i = 0; i< this.usuarios.length; i++){
+      let usuarioActual = this.usuarios[i];
+      if(usuarioActual.usuario === nombreDeUsuario){ //Ver aca los nombres de nuestras variables principalmente nombreDeUsuario
+        estaRepetido = true;
+      }
+    }
+    return estaRepetido;
+  }
+
+// Ver la clase o ver luego que no esta referenciado que el profe ya estaba yendo al palo
+  loginValido(nombreDeUsuario, pass){   //ver variables nuestras comparadas con las del profe que segundo esta alguna mal!
+    let loginOk = false;
+    for(let i = 0; i< this.usuarios.length && !loginOk; i++){
+      let usuarioActual = this.usuarios[i];
+      if(usuarioActual.usuario.ToLowerCase() === nombreDeUsuario.ToLowerCase() && usuarioActual.password === pas){ //Ver aca los nombres de nuestras variables principalmente nombreDeUsuario
+        loginOk = true;
+      }
+    }
+    return loginOk;
+  }
 }
 
 class Usuario {
@@ -17,3 +41,7 @@ class Usuario {
     this.contraseña = contraseña;
   }
 }
+
+
+// Cuando entras al usuario logeado osea ensista.. que te muestre arriba el usuario logueado.
+// tres div principales.. uno de registro y logeo, otro de sensista y otro del invitado.
